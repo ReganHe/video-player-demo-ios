58d3990d9a4517a522e0652efbfbe1dcf5b7bc2e
ReganHe <heoo547627@126.com>
2023-11-02 15:33:20 +0800

    移除Main.Storyboard

--- videoplayerdemo/SceneDelegate.m
+++ videoplayerdemo/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "ViewController.h"
 
 @interface SceneDelegate ()
 
@@ -18,6 +19,11 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    UIWindowScene *windowScene=(UIWindowScene *)scene;
+    _window=[[UIWindow alloc] initWithFrame:windowScene.coordinateSpace.bounds];
+    _window.windowScene=windowScene;
+    [_window setRootViewController:[[ViewController alloc] init]];
+    [_window makeKeyAndVisible];
 }
 
 
